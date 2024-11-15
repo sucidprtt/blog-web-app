6cd513a6027e8b9b52ab675c58661d4b7addd930
sucidprtt <pratiwisucidwii@gmail.com>
2024-11-16 03:45:02 +0700

    feat: 7.13 Header n Footer Component

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,29 +1,35 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home, PostDetail, PostForm } from "../pages";
-import { PostItem } from "../components";
+import { PostItem, MainLayout } from "../components";
 
 export const routes = createBrowserRouter([
     {
         path: '/',
-        element: <Home />
-    },
-    {
-        path: '/post-form',
-        element: <PostForm />
-    },
-    {
-        path: '/post-detail',
-        element: <PostDetail />
-    },
-    // DEBUG
-    {
-        path: 'post-item',
-        element: <PostItem 
-            image="https://picsum.photos/800"
-            title="Image Title"
-            name="John Doe"
-            date="18 Jul 2024"
-            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fringilla metus sapien, ac lacinia ante maximus id. Aenean id lorem elit. Duis finibus a mauris ac fringilla."
-        />
+        element: <MainLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/post-form',
+                element: <PostForm />
+            },
+            {
+                path: '/post-detail',
+                element: <PostDetail />
+            },
+            // DEBUG
+            {
+                path: 'post-item',
+                element: <PostItem 
+                    image="https://picsum.photos/800"
+                    title="Image Title"
+                    name="John Doe"
+                    date="18 Jul 2024"
+                    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fringilla metus sapien, ac lacinia ante maximus id. Aenean id lorem elit. Duis finibus a mauris ac fringilla."
+                />
+            }
+        ]
     }
 ])
